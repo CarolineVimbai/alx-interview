05ad4bf1ac05ea8f61feee07e2cc62b8065eaf80
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-10-07 14:48:52 +0000

    Task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1]  # First element in each row is always 1
+        if triangle:  # If there are previous rows, calculate the current row
+            prev_row = triangle[-1]
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+            row.append(1)  # Last element in each row is always 1
+        triangle.append(row)
+
+    return triangle
