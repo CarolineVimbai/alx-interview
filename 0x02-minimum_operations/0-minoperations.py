6580d321cbd8147b14c13c773f702757f200b9f2
CarolineVimbai <carolinevimbaichamboko@gmail.com>
2023-10-23 20:08:33 +0000

    Task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return i + minOperations(n // i)
+    return n
+
+# Example usage
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
